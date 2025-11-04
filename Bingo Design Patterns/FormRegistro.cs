db29bdb19027b225df470886648795d1bdcc9c46
nicollecg12 <nicollecarvajalgomez@gmail.com>
2025-11-03 19:47:09 -0500

    Interfaz de registro de usuarios Funcional

--- Bingo Design Patterns/FormRegistro.cs
+++ Bingo Design Patterns/FormRegistro.cs
@@ -15,6 +15,10 @@ namespace Bingo_Design_Patterns
         public FormRegistro()
         {
             InitializeComponent();
+            cboTipoUsuario.Items.Add("Jugador");
+            cboTipoUsuario.Items.Add("Administrador");
+            cboTipoUsuario.SelectedIndex = -1;
+
         }
 
         private void FormRegistro_Load(object sender, EventArgs e)
@@ -24,8 +28,53 @@ namespace Bingo_Design_Patterns
 
         private void btnRegistrarse_Click(object sender, EventArgs e)
         {
-            FormInicio formInicio = new FormInicio();
-            formInicio.Show();
+            string tipo = cboTipoUsuario.Text;
+            string nombre = txtNombre.Text;
+            string numero = txtNumeroTelefonico.Text;
+            string user = txtNombreUsuario.Text;
+            string contraseña = txtContraseña.Text;
+
+
+
+            try
+            {
+                if (!long.TryParse(numero, out _))
+                {
+                    MessageBox.Show("El número telefonico solo puede contener caracteres numéricos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                if (int.TryParse(nombre, out _))
+                {
+                    MessageBox.Show("El nombre no debe contener números", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+
+                }
+                foreach (var users in GestorUsuarios.Instancia.ObtenerUsuarios())
+                {
+                    if (users.User == user)
+                    {
+                        MessageBox.Show("Ya existe ese nombre de usuario", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+                }
+                Usuario nuevo = UsuarioFactory.CrearUsuario(nombre, numero, user, contraseña, tipo);
+                GestorUsuarios.Instancia.AgregarUsuario(nuevo);
+                MessageBox.Show(nuevo.VerificarCreacion(), "Atención",MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                FormInicio formInicio = new FormInicio();
+                formInicio.Show();
+                this.Hide();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error: " + error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            FormInicio frm = new FormInicio();
+            frm.Show();
             this.Hide();
         }
     }

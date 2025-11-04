1e663171f48675aa22b1d4677e9c0f264f5edce6
nicollecg12 <nicollecarvajalgomez@gmail.com>
2025-11-03 19:46:15 -0500

    Clase derivada Administrador

---
+++ Bingo Design Patterns/Administrador.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bingo_Design_Patterns
+{
+    public class Administrador: Usuario
+    {
+        public Administrador(string nombre, string numero, string user, string contraseña) : base(nombre, numero, user, contraseña)
+        {
+        }
+        public override string VerificarCreacion()
+        {
+            return "¡Administrador registrado exitosamente!";
+        }
+    }
+}

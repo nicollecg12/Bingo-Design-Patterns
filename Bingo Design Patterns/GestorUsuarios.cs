c5cf682ad4640f06a1ca58247ebc7a00902ff70d
nicollecg12 <nicollecarvajalgomez@gmail.com>
2025-11-03 19:46:46 -0500

    Uso y creación de Singleton

---
+++ Bingo Design Patterns/GestorUsuarios.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bingo_Design_Patterns
+{
+    public class GestorUsuarios
+    {
+        public static GestorUsuarios _instancia;
+        private List<Usuario> usuarios;
+
+        private GestorUsuarios() 
+        { 
+        usuarios = new List<Usuario>();
+        }
+
+        public static GestorUsuarios Instancia 
+        {
+            get 
+            {
+                if (_instancia == null) 
+                { 
+                _instancia = new GestorUsuarios();
+                }
+                return _instancia;
+            }
+        }
+
+        public void AgregarUsuario(Usuario usuario)
+        {
+            usuarios.Add(usuario);
+        }
+
+        public List<Usuario> ObtenerUsuarios()
+        {
+            return usuarios;
+        }
+    }
+}

6fc33179cef2c01400a2ccb361e2e9b9f2d269aa
nicollecg12 <nicollecarvajalgomez@gmail.com>
2025-11-03 19:46:25 -0500

    Uso de patrón Factory

---
+++ Bingo Design Patterns/UsuarioFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bingo_Design_Patterns
+{
+    public class UsuarioFactory
+    {
+        public static Usuario CrearUsuario(string nombre, string numero, string user, string contraseña, string tipo) 
+        {
+            if (tipo == "Jugador")
+            {
+                return new Jugador(nombre, numero, user, contraseña);
+            }
+            if (tipo == "Administrador")
+            {
+                return new Administrador(nombre, numero, user, contraseña);
+            }
+            else
+            {
+                throw new ArgumentException("Tipo de usuario no válido");
+            }
+        }
+    }
+}

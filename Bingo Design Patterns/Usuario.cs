d1e51310b0eddfe9813e3af69328e3455d5e338f
nicollecg12 <nicollecarvajalgomez@gmail.com>
2025-11-03 19:45:52 -0500

    Clase base Usuario

---
+++ Bingo Design Patterns/Usuario.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bingo_Design_Patterns
+{
+    public abstract class Usuario
+    {
+        public string Nombre { get; set; }
+        public string Numero { get; set; }
+        public string User { get; set; }
+        public string Contraseña { get; set; }
+
+        public Usuario(string nombre, string numero, string user, string contraseña)
+        {
+
+            if (string.IsNullOrWhiteSpace(nombre) || string.IsNullOrWhiteSpace(user)|| string.IsNullOrWhiteSpace(contraseña)|| string.IsNullOrWhiteSpace(numero))
+            {
+                throw new ArgumentException("No puede dejar campos vacíos");
+            }
+            if (contraseña.Length < 8)
+            {
+                throw new ArgumentException("La contraseña debe contener al menos 8 caracteres");
+            }
+            if (user.Length < 3)
+            {
+                throw new ArgumentException("El nombre de usuario debe contener al menos 3 caracteres");
+            }
+           
+
+            Nombre = nombre;
+            Numero = numero;
+            User = user;
+            Contraseña = contraseña;
+        }
+
+        public abstract string VerificarCreacion();
+        
+    }
+}

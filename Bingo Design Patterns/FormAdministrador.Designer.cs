f44ea8a0a58a1bcf1f425f87f286c7e2abc8224f
nicollecg12 <nicollecarvajalgomez@gmail.com>
2025-11-24 17:36:43 -0500

    Formulario Administrador Agregado

---
+++ Bingo Design Patterns/FormAdministrador.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Bingo_Design_Patterns
+{
+    partial class FormAdministrador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAdministrador));
+            this.btnGenerarReporte = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEliminarUsuario = new System.Windows.Forms.Button();
+            this.btnAdministrarPalabras = new System.Windows.Forms.Button();
+            this.btnAgregarAdministrador = new System.Windows.Forms.Button();
+            this.dgvListaUsuarios = new System.Windows.Forms.DataGridView();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Usuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Contraseña = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Telefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Rol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PartidasGanadas = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListaUsuarios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGenerarReporte
+            // 
+            this.btnGenerarReporte.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.btnGenerarReporte.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerarReporte.ForeColor = System.Drawing.Color.White;
+            this.btnGenerarReporte.Location = new System.Drawing.Point(699, 319);
+            this.btnGenerarReporte.Name = "btnGenerarReporte";
+            this.btnGenerarReporte.Size = new System.Drawing.Size(172, 53);
+            this.btnGenerarReporte.TabIndex = 13;
+            this.btnGenerarReporte.Text = "Generar reporte";
+            this.btnGenerarReporte.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.SteelBlue;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(360, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 30);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Administrador";
+            // 
+            // btnEliminarUsuario
+            // 
+            this.btnEliminarUsuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnEliminarUsuario.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminarUsuario.ForeColor = System.Drawing.Color.White;
+            this.btnEliminarUsuario.Location = new System.Drawing.Point(699, 163);
+            this.btnEliminarUsuario.Name = "btnEliminarUsuario";
+            this.btnEliminarUsuario.Size = new System.Drawing.Size(172, 33);
+            this.btnEliminarUsuario.TabIndex = 11;
+            this.btnEliminarUsuario.Text = "Eliminar usuario";
+            this.btnEliminarUsuario.UseVisualStyleBackColor = false;
+            // 
+            // btnAdministrarPalabras
+            // 
+            this.btnAdministrarPalabras.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnAdministrarPalabras.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdministrarPalabras.ForeColor = System.Drawing.Color.White;
+            this.btnAdministrarPalabras.Location = new System.Drawing.Point(699, 260);
+            this.btnAdministrarPalabras.Name = "btnAdministrarPalabras";
+            this.btnAdministrarPalabras.Size = new System.Drawing.Size(172, 53);
+            this.btnAdministrarPalabras.TabIndex = 10;
+            this.btnAdministrarPalabras.Text = "Administrar palabras";
+            this.btnAdministrarPalabras.UseVisualStyleBackColor = false;
+            // 
+            // btnAgregarAdministrador
+            // 
+            this.btnAgregarAdministrador.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnAgregarAdministrador.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregarAdministrador.ForeColor = System.Drawing.Color.White;
+            this.btnAgregarAdministrador.Location = new System.Drawing.Point(699, 202);
+            this.btnAgregarAdministrador.Name = "btnAgregarAdministrador";
+            this.btnAgregarAdministrador.Size = new System.Drawing.Size(172, 52);
+            this.btnAgregarAdministrador.TabIndex = 9;
+            this.btnAgregarAdministrador.Text = "Agregar administrador";
+            this.btnAgregarAdministrador.UseVisualStyleBackColor = false;
+            // 
+            // dgvListaUsuarios
+            // 
+            this.dgvListaUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListaUsuarios.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre,
+            this.Usuario,
+            this.Contraseña,
+            this.Telefono,
+            this.Rol,
+            this.PartidasGanadas});
+            this.dgvListaUsuarios.Location = new System.Drawing.Point(26, 152);
+            this.dgvListaUsuarios.Name = "dgvListaUsuarios";
+            this.dgvListaUsuarios.Size = new System.Drawing.Size(640, 220);
+            this.dgvListaUsuarios.TabIndex = 8;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            // 
+            // Usuario
+            // 
+            this.Usuario.HeaderText = "Usuario";
+            this.Usuario.Name = "Usuario";
+            // 
+            // Contraseña
+            // 
+            this.Contraseña.HeaderText = "Contraseña";
+            this.Contraseña.Name = "Contraseña";
+            // 
+            // Telefono
+            // 
+            this.Telefono.HeaderText = "Telefono";
+            this.Telefono.Name = "Telefono";
+            // 
+            // Rol
+            // 
+            this.Rol.HeaderText = "Rol";
+            this.Rol.Name = "Rol";
+            // 
+            // PartidasGanadas
+            // 
+            this.PartidasGanadas.HeaderText = "Partidas Ganadas";
+            this.PartidasGanadas.Name = "PartidasGanadas";
+            // 
+            // FormAdministrador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(902, 435);
+            this.Controls.Add(this.btnGenerarReporte);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEliminarUsuario);
+            this.Controls.Add(this.btnAdministrarPalabras);
+            this.Controls.Add(this.btnAgregarAdministrador);
+            this.Controls.Add(this.dgvListaUsuarios);
+            this.Name = "FormAdministrador";
+            this.Text = "FormAdministrador";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListaUsuarios)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerarReporte;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEliminarUsuario;
+        private System.Windows.Forms.Button btnAdministrarPalabras;
+        private System.Windows.Forms.Button btnAgregarAdministrador;
+        private System.Windows.Forms.DataGridView dgvListaUsuarios;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Usuario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Contraseña;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Telefono;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Rol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PartidasGanadas;
+    }
+}
---
+++ Bingo Design Patterns/FormAdministrador.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bingo_Design_Patterns
+{
+    public partial class FormAdministrador : Form
+    {
+        public FormAdministrador()
+        {
+            InitializeComponent();
+        }
+    }
+}
